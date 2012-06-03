23d38ffe151ae06a43e70e0bf900a1ac21affe6a
Jörg Richter <jri@deepamehta.de>
2012-06-03 16:49:27 +0200

    Support for retrieving multi-facets.
    New service call: getFacets().

--- src/main/java/de/deepamehta/plugins/facets/FacetsPlugin.java
+++ src/main/java/de/deepamehta/plugins/facets/FacetsPlugin.java
@@ -14,6 +14,7 @@ import de.deepamehta.core.service.ClientState;
 import de.deepamehta.core.service.Directives;
 import de.deepamehta.core.service.Plugin;
 
+import java.util.Set;
 import java.util.logging.Logger;
 
 
@@ -35,7 +36,21 @@ public class FacetsPlugin extends Plugin implements FacetsService {
 
 
     @Override
-    public void associateWithFacetType(long topicId, String facetTypeUri) {
+    public Topic getFacet(Topic topic, String facetTypeUri) {
+        // ### TODO: integrity check: is the topic an instance of that facet type?
+        return fetchChildTopic(topic, getAssocDef(facetTypeUri), true);     // fetchComposite=true
+    }
+
+    @Override
+    public Set<RelatedTopic> getFacets(Topic topic, String facetTypeUri) {
+        // ### TODO: integrity check: is the topic an instance of that facet type?
+        return fetchChildTopics(topic, getAssocDef(facetTypeUri), true);    // fetchComposite=true
+    }
+
+    // ---
+
+    @Override
+    public void addFacetTypeToTopic(long topicId, String facetTypeUri) {
         dms.createAssociation(new AssociationModel("dm4.core.instantiation", 
             new TopicRoleModel(topicId,      "dm4.core.instance"),
             new TopicRoleModel(facetTypeUri, "dm4.facets.facet")), null);   // clientState=null
@@ -49,21 +64,9 @@ public class FacetsPlugin extends Plugin implements FacetsService {
         topic.updateChildTopic(getAssocDef(facetTypeUri), facet, clientState, directives);
     }
 
-    @Override
-    public Topic getFacet(Topic topic, String facetTypeUri) {
-        // ### TODO: integrity check: is the topic an instance of that facet type?
-        // ### TODO: many cardinality
-        return fetchChildTopic(topic, getAssocDef(facetTypeUri), true);     // fetchComposite=true
-    }
-
 
 
     // ------------------------------------------------------------------------------------------------- Private Methods
-
-    private AssociationDefinition getAssocDef(String facetTypeUri) {
-        // Note: a facet type has exactly *one* association definition
-        return dms.getTopicType(facetTypeUri, null).getAssocDefs().values().iterator().next();
-    }
 
     /**
      * Fetches and returns a child topic or <code>null</code> if no such topic extists.
@@ -79,5 +82,28 @@ public class FacetsPlugin extends Plugin implements FacetsService {
         //
         return topic.getRelatedTopic(assocTypeUri, myRoleTypeUri, othersRoleTypeUri, othersTopicTypeUri,
             fetchComposite, false, null);
+    }
+
+    /**
+     * Fetches and returns child topics.
+     * <p>
+     * ### Note: There is a principal copy in AttachedDeepaMehtaObject but here the precondition is different:
+     * The given AssociationDefinition is not necessarily part of the given topic's type.
+     */
+    private Set<RelatedTopic> fetchChildTopics(Topic topic, AssociationDefinition assocDef, boolean fetchComposite) {
+        String assocTypeUri       = assocDef.getInstanceLevelAssocTypeUri();
+        String myRoleTypeUri      = assocDef.getWholeRoleTypeUri();
+        String othersRoleTypeUri  = assocDef.getPartRoleTypeUri();
+        String othersTopicTypeUri = assocDef.getPartTopicTypeUri();
+        //
+        return topic.getRelatedTopics(assocTypeUri, myRoleTypeUri, othersRoleTypeUri, othersTopicTypeUri,
+            fetchComposite, false, 0, null).getItems();
+    }
+
+    // ---
+
+    private AssociationDefinition getAssocDef(String facetTypeUri) {
+        // Note: a facet type has exactly *one* association definition
+        return dms.getTopicType(facetTypeUri, null).getAssocDefs().values().iterator().next();
     }
 }
